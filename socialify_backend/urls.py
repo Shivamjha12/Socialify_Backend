f0095ee0fe153967adad4abd66cde0edadf196ed
Shivamjha12 <Jhasapna544@gmail.com>
2024-03-02 16:44:40 +0530

    Accounts

--- socialify_backend/urls.py
+++ socialify_backend/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-user/', include('accounts.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# added new static
